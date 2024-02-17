f063298d868a3f5d05c9bc10bb8bffd571beee61
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2024-02-17 13:49:27 -0300

    feature: representing data in human readable way

--- football_backend/topscorers/views.py
+++ football_backend/topscorers/views.py
@@ -6,9 +6,10 @@ from django.http import JsonResponse
 
 API_KEY = os.environ.get('API_KEY')
 
+
 def get_top_scorers(request, league_id):
     url = f"https://apiv3.apifootball.com/?action=get_topscorers&league_id={league_id}&APIkey={API_KEY}"
-  
+
     response = requests.get(url)
     data = response.json()
     
@@ -17,4 +18,3 @@ def get_top_scorers(request, league_id):
     else:
         json_data = json.dumps(data)
         return JsonResponse(json_data, safe=False)
-
--- football_frontend/src/App.js
+++ football_frontend/src/App.js
@@ -1,28 +1,41 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [jsonData, setJsonData] = useState('');
+  const [players, setPlayers] = useState([]);
 
   useEffect(() => {
-    const fetchGames = async () => {
+    const fetchPlayers = async () => {
       try {
-        const response = await fetch('http://localhost:8000/topscorers/151/'); 
+        const response = await fetch('http://localhost:8000/topscorers/152/');
         const data = await response.json();
-        setJsonData(JSON.stringify(data, null, 2)); 
+        const parsedData = JSON.parse(data);
+        setPlayers(parsedData);
       } catch (error) {
-        console.error('Error fetching games:', error);
+        console.error('Error fetching players:', error);
       }
     };
-    fetchGames();
+    fetchPlayers();
   }, []);
 
   return (
     <div>
       <h1>Top Scorers</h1>
-      <pre>{jsonData}</pre>
+      <div>
+        {players.map((player, index) => {
+          return (
+            <div key={index}>
+              <p>Player Name: {player.player_name}</p>
+              <p>Team Name: {player.team_name}</p>
+              <p>Goals: {player.goals}</p>
+              <p>Assists: {player.assists}</p>
+              <p>Penalty Goals: {player.penalty_goals}</p>
+              <hr />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
 
 export default App;
-
